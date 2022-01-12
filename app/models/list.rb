11bb876f87bb9fdb93af59575ba082a23b518e74
EC2 Default User <ec2-user@ip-172-31-88-232.ec2.internal>
2022-01-12 03:16:13 +0000

    [start]3-8

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,9 @@
 class List < ApplicationRecord
   has_one_attached :image
+
+#   バリデーション設定
+validates :title, presence: true
+validates :body, presence: true
+validates :image, presence: true
+
 end
